457a8382481b4e7f3b269b5f7d8c6d0875a9c7e6
Pragati Priyadarshini <66525531+Pragati0302@users.noreply.github.com>
2020-10-27 11:39:24 +0530

    update carousel.js
    Changed the code

--- ten_social_media/src/Components/corousel.js
+++ ten_social_media/src/Components/corousel.js
@@ -1,36 +1,34 @@
 import React from 'react';
-import { Fade } from 'react-slideshow-image';
-import 'react-slideshow-image/dist/styles.css'
- 
-const fadeImages = [
-  '',
-  '',
-  ''
+import { Slide } from 'react-slideshow-image';
+
+const slideImages = [
+  'https://images.unsplash.com/photo-1603632076161-5836b146638c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+  'https://images.unsplash.com/photo-1603702250862-b2e56e01a5c8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+  'https://images.unsplash.com/photo-1603729210683-21a3763c1233?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
 ];
- 
+
 const Slideshow = () => {
-  return (
-    <div className="slide-container">
-      <Fade>
-        <div className="each-fade">
-          <div className="image-container">
-            <img src={fadeImages[0]} />
+    return (
+      <div>
+        <Slide easing="ease">
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
+              <span>Slide 1</span>
+            </div>
           </div>
-          <h2>First Slide</h2>
-        </div>
-        <div className="each-fade">
-          <div className="image-container">
-            <img src={fadeImages[1]} />
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
+              <span>Slide 2</span>
+            </div>
           </div>
-          <h2>Second Slide</h2>
-        </div>
-        <div className="each-fade">
-          <div className="image-container">
-            <img src={fadeImages[2]} />
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+              <span>Slide 3</span>
+            </div>
           </div>
-          <h2>Third Slide</h2>
-        </div>
-      </Fade>
-    </div>
-  )
-}
+        </Slide>
+      </div>
+    )
+};
+
+export default Slideshow;
